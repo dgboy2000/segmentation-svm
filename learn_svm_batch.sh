61b4d23ac59edbc1f1d8b798423aebc20d9fdb74
Pierre-Yves Baudin <baudinpy@service0.ice.ecp.fr>
2012-08-17 15:25:13 +0200

    added back cluster run script

---
+++ learn_svm_batch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# shell
+#PBS -S /bin/bash
+
+# name of the job
+#PBS -N learnbatch
+
+# output
+#PBS -o log_learn_svm_batch.txt
+
+# standard error output
+#PBS -j oe
+
+# max execution time
+#PBS -l walltime=20:00:00
+
+# ressources blocs to allocate
+#PBS -l select=1:ncpus=1:mem=12gb
+
+# queueName
+#PBS -q iceq
+
+cd $HOME/segmentation-svm/
+
+python learn_svm_batch.py
+
