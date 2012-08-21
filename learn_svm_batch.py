c7de198ec945ad5ab7face52a56750baf9c16e85
pybaudin <pybaudin@gmail.com>
2012-08-21 16:30:43 +0200

    set parameters for run on igloo

--- learn_svm_batch.py
+++ learn_svm_batch.py
@@ -74,8 +74,8 @@ class SVMSegmenter(object):
         self.labelset = np.asarray([0,13,14,15,16])
         
         # self.training_vols = ['02/'] ## debug
-        self.training_vols = ['02/','03/'] ## debug
-        # self.training_vols = config.vols
+        # self.training_vols = ['02/','03/'] ## debug
+        self.training_vols = config.vols
 
         
         ## parameters for rw learning
@@ -111,8 +111,8 @@ class SVMSegmenter(object):
         ## weight functions
         self.weight_functions = {
             'std_b10'     : lambda im: wflib.weight_std(im, beta=10),
-            # 'std_b50'     : lambda im: wflib.weight_std(im, beta=50),
-            # 'std_b100'    : lambda im: wflib.weight_std(im, beta=100),
+            'std_b50'     : lambda im: wflib.weight_std(im, beta=50),
+            'std_b100'    : lambda im: wflib.weight_std(im, beta=100),
             # 'inv_b100o1'  : lambda im: wflib.weight_inv(im, beta=100, offset=1),
             # 'pdiff_r1b50' : lambda im: wflib.weight_patch_diff(im, r0=1, beta=50),
             # 'pdiff_r1b100': lambda im: wflib.weight_patch_diff(im, r0=1, beta=100),
