ad889bec7e66e93cd014dd58a982753ee9ae13c3
Danny Goodman <goodman.danny@gmail.com>
2012-08-23 17:36:30 +0200

    try doubling the memory per process to see if that prevents the root process from being killed

--- learn_svm_batch.sh
+++ learn_svm_batch.sh
@@ -17,7 +17,7 @@
 
 # resources blocs to allocate
 # mpiprocs is num. of train img + 1
-#PBS -l select=8:ncpus=4:mpiprocs=4:mem=22gb
+#PBS -l select=16:ncpus=2:mpiprocs=2:mem=22gb
 
 # queueName
 #PBS -q iceq
