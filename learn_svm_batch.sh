4dd211b9001e2329fae1f200e5cfb8310458cb26
Danny Goodman <goodman.danny@gmail.com>
2012-08-29 20:19:29 +0200

    params for a run on the 72gb q

--- learn_svm_batch.sh
+++ learn_svm_batch.sh
@@ -23,17 +23,17 @@
 ##PBS -l select=1:ncpus=12:mpiprocs=5:mem=44gb
 ##PBS -l walltime=23:59:00
 #
-## Params for icemem72gbq
-##PBS -l select=1:ncpus=12:mpiprocs=7:mem=68gb
-##PBS -l walltime=23:59:00
+# Params for icemem72gbq
+#PBS -l select=1:ncpus=12:mpiprocs=7:mem=68gb
+#PBS -l walltime=23:59:00
 #
 ## Params for icepar156q
 ##PBS -l select=11:ncpus=12:mpiprocs=3:mem=22gb
 ##PBS -l walltime=03:59:00
-
-# Params for icetestq
-#PBS -l select=1:ncpus=12:mpiprocs=3:mem=22gb
-#PBS -l walltime=00:19:59
+#
+## Params for icetestq
+##PBS -l select=1:ncpus=12:mpiprocs=3:mem=22gb
+##PBS -l walltime=00:19:59
 
 # queueName
 #PBS -q iceq
