0addde025d8ecbd633c0b63bc3984cde52e9288a
Danny Goodman <goodman.danny@gmail.com>
2012-08-29 17:51:55 +0200

    add params that can be commented in to try all of the igloo queues. start with the testq params

--- learn_svm_batch.sh
+++ learn_svm_batch.sh
@@ -12,12 +12,28 @@
 # standard error output
 #PBS -j oe
 
-# max execution time
-#PBS -l walltime=03:59:00
+## max execution time
+##PBS -l walltime=03:59:00
 
-# resources blocs to allocate
-# mpiprocs is num. of train img + 1
+## resources blocs to allocate
+## mpiprocs is num. of train img + 1
+##PBS -l select=11:ncpus=12:mpiprocs=3:mem=22gb
+
+## Params for icemem48gbq
+##PBS -l select=1:ncpus=12:mpiprocs=5:mem=44gb
+##PBS -l walltime=23:59:00
+#
+## Params for icemem72gbq
+##PBS -l select=1:ncpus=12:mpiprocs=7:mem=68gb
+##PBS -l walltime=23:59:00
+#
+## Params for icepar156q
+##PBS -l select=11:ncpus=12:mpiprocs=3:mem=22gb
+##PBS -l walltime=03:59:00
+
+# Params for icetestq
 #PBS -l select=11:ncpus=12:mpiprocs=3:mem=22gb
+#PBS -l walltime=00:19:59
 
 # queueName
 #PBS -q iceq
