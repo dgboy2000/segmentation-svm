63ec1858c76f0e55f87ecce8ece3e39778ae2d9a
Danny Goodman <goodman.danny@gmail.com>
2012-08-29 20:37:47 +0200

    fix number of processes in command line

--- learn_svm_batch.sh
+++ learn_svm_batch.sh
@@ -22,22 +22,26 @@
 # Params for icemem48gbq
 #PBS -l select=1:ncpus=12:mpiprocs=5:mem=44gb
 #PBS -l walltime=23:59:00
+NP=5
 #
 ## Params for icemem72gbq
 ##PBS -l select=1:ncpus=12:mpiprocs=7:mem=68gb
 ##PBS -l walltime=23:59:00
+# NP=7
 #
 ## Params for icepar156q
 ##PBS -l select=11:ncpus=12:mpiprocs=3:mem=22gb
 ##PBS -l walltime=03:59:00
+# NP=33
 #
 ## Params for icetestq
 ##PBS -l select=1:ncpus=12:mpiprocs=3:mem=22gb
 ##PBS -l walltime=00:19:59
+# NP=3
 
 # queueName
 #PBS -q iceq
 
 cd $HOME/segmentation-svm/
-mpirun -np 33 python learn_svm_batch.py --parallel
+mpirun -np $NP python learn_svm_batch.py --parallel
 
