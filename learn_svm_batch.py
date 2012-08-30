240b5a2540744c91f03306c9ab99a546baec9a8a
U-JACQUES\baudinpy <baudinpy@JACQUES.(none)>
2012-08-30 20:19:16 +0200

    constrained inference for debugging

--- learn_svm_batch.py
+++ learn_svm_batch.py
@@ -118,7 +118,8 @@ class SVMSegmenter(object):
             'rtol': 1e-6,
             'maxiter': 1e3,
             'per_label':True,
-            'optim_solver':'unconstrained',
+            # 'optim_solver':'unconstrained',
+            'optim_solver':'constrained', ## debug
             }
             
         ## svm params
