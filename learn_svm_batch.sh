44fa71d386329e9e90c133c8c6e6aa0e278fc065
Pierre-Yves Baudin <baudinpy@service0.ice.ecp.fr>
2012-08-30 13:35:18 +0200

    baseline on igloo test

--- learn_svm_batch.sh
+++ learn_svm_batch.sh
@@ -19,10 +19,10 @@
 ## mpiprocs is num. of train img + 1
 ##PBS -l select=11:ncpus=12:mpiprocs=3:mem=22gb
 
-# Params for icemem48gbq
-#PBS -l select=1:ncpus=12:mpiprocs=6:mem=44gb
-#PBS -l walltime=23:59:00
-NP=6
+## Params for icemem48gbq
+##PBS -l select=1:ncpus=12:mpiprocs=6:mem=44gb
+##PBS -l walltime=23:59:00
+#NP=6
 #
 ## Params for icemem72gbq
 ##PBS -l select=1:ncpus=12:mpiprocs=7:mem=68gb
@@ -30,9 +30,9 @@ NP=6
 # NP=7
 #
 ## Params for icepar156q
-##PBS -l select=11:ncpus=12:mpiprocs=3:mem=22gb
-##PBS -l walltime=03:59:00
-# NP=33
+#PBS -l select=11:ncpus=12:mpiprocs=3:mem=22gb
+#PBS -l walltime=03:59:00
+NP=33
 #
 ## Params for icetestq
 ##PBS -l select=1:ncpus=12:mpiprocs=6:mem=22gb
@@ -43,5 +43,7 @@ NP=6
 #PBS -q iceq
 
 cd $HOME/segmentation-svm/
-mpirun -np $NP python learn_svm_batch.py --parallel --latent
+mpirun -np $NP python learn_svm_batch.py --parallel
+#mpirun -np $NP python learn_svm_batch.py --parallel --loss laplacian
+#mpirun -np $NP python learn_svm_batch.py --parallel --latent
 
